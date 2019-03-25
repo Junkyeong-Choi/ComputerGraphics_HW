63c2109519f8aa8318ffc0675064b7974055fc0c
JunGyeong Choi <adwwsd@naver.com>
2019-03-25 17:59:39 +0900

    Add vibration a logic to the character class, but didn't apply it to update method of the game class

--- src/characterObject.cpp
+++ src/characterObject.cpp
@@ -1,4 +1,8 @@
 #include "characterObject.h"
+#define DURATION_OF_VIBRATION 500
+#define TAIL_PROXIMAL_ANGLE_LIMIT 15
+#define TAIL_DISTAL_ANGLE_LIMIT 15
+#define EAR_ANGLE_LIMIT 15
 
 void CharacterObject::move(int delta) {
 	glm::vec2 displacement = velocity * (float)delta;
@@ -9,9 +13,9 @@ void CharacterObject::move(int delta) {
 	if (position.x + size.x > 192)
 		position.x = 192 - size.x;
 
-	tailProximalAngle += tailProximalAngleSpeed * delta;
-	tailDistalAngle += tailDistalAngleSpeed * delta;
-	earAngle += earAngleSpeed * delta;
+	tailProximalAngle += tailProximalAngleVelocity * delta;
+	tailDistalAngle += tailDistalAngleVelocity * delta;
+	earAngle += earAngleVelocity * delta;
 
 	if (tailProximalAngle > 360)
 		tailProximalAngle -= 360;
@@ -19,4 +23,22 @@ void CharacterObject::move(int delta) {
 		tailDistalAngle -= 360;
 	if (earAngle > 360)
 		earAngle -= 360;
+
+	durationOfVibration -= delta;
+	if (durationOfVibration > 0) {
+		tailProximalAngle += tailProximalAngleVelocity * delta;
+		tailDistalAngle += tailDistalAngleVelocity * delta;
+		earAngle += earAngleVelocity * delta;
+
+		if (tailProximalAngle > TAIL_PROXIMAL_ANGLE_LIMIT || tailProximalAngle < -TAIL_PROXIMAL_ANGLE_LIMIT)
+			tailProximalAngleVelocity = -tailProximalAngleVelocity;
+		if (tailDistalAngle > TAIL_DISTAL_ANGLE_LIMIT || tailDistalAngle < -TAIL_DISTAL_ANGLE_LIMIT)
+			tailDistalAngleVelocity = -tailDistalAngleVelocity;
+		if (earAngle > EAR_ANGLE_LIMIT || earAngle < -EAR_ANGLE_LIMIT)
+			earAngleVelocity = -earAngleVelocity;
+	}
+}
+
+void CharacterObject::handleCollision(int delta) {
+	durationOfVibration = DURATION_OF_VIBRATION;
 }
