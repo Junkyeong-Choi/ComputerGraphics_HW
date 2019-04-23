1b74694d2a63afe779a46f04132e5741a01f8947
Beomsu Lim <lbs1163@postech.ac.kr>
2019-04-23 10:46:54 +0900

    Modify model matrix for pikachu

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -24,7 +24,7 @@ void Renderer::render(MovableCubeObject& player1, MovableCubeObject& player2, Ba
 
 	shader.use();
 
-	glm::mat4 projection = glm::perspective(glm::radians(60.0f), (float)width / (float)height, 0.1f, 1000.0f);
+	glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 1000.0f);
 	
 	glm::vec3 eye, center, up;
 	if (viewmode == VIEW_CHARACTER_EYE) {
@@ -50,26 +50,36 @@ void Renderer::render(MovableCubeObject& player1, MovableCubeObject& player2, Ba
 	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	float player1DirectionAngle = player1.getDirectionAngle().x;
 	float player2DirectionAngle = player2.getDirectionAngle().x;
-	   
+
+	glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	rotation = glm::rotate(rotation, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
 	glm::mat4 model = glm::mat4(1.0f);
+	// finally, get pikachu to its position
 	model = glm::translate(model, player1.getPosition());
-	model = glm::rotate(model, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-	model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-	model = glm::rotate(model, player1DirectionAngle, glm::vec3(0.0f, 1.0f, 0.0f));
+	// fourth, rotate pikachu to look at the right direction
+	model = glm::rotate(model, player1DirectionAngle, glm::vec3(0.0f, 0.0f, 1.0f));
+	// third, apply scaling to match model's size to bounding box (should be done after rotating model)
+	model = glm::scale(model, player1.getSize() / glm::vec3(rotation * glm::vec4(pikachu.getSize(), 1.0f)));
+	// second, rotate pikachu so that the head towards to the positive z-axis
+	model = model * rotation;
+	// first, align pikachu's left-bottom-back point to origin
 	model = glm::translate(model, -pikachu.getMin());
-	model = glm::scale(model, player1.getSize() / pikachu.getSize());
 	shader.setMat4("model", model);
 
 	pikachu.Draw(shader);
 
 	model = glm::mat4(1.0f);
+	// finally, get pikachu to its position
 	model = glm::translate(model, player2.getPosition());
-	model = glm::rotate(model, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-	model = glm::rotate(model, glm::radians(270.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-	model = glm::rotate(model, -player2DirectionAngle, glm::vec3(0.0f, 1.0f, 0.0f));
+	// fourth, rotate pikachu to look at the right direction
+	model = glm::rotate(model, glm::radians(180.0f) - player2DirectionAngle, glm::vec3(0.0f, 0.0f, 1.0f));
+	// third, apply scaling to match model's size to bounding box (should be done after rotating model)
+	model = glm::scale(model, player2.getSize() / glm::vec3(rotation * glm::vec4(pikachu.getSize(), 1.0f)));
+	// second, rotate pikachu so that the head towards to the positive z-axis
+	model = model * rotation;
+	// first, align pikachu's left-bottom-back point to origin
 	model = glm::translate(model, -pikachu.getMin());
-	model = glm::scale(model, player2.getSize() / pikachu.getSize());
 	shader.setMat4("model", model);
 
 	pikachu.Draw(shader);
